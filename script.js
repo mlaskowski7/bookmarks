c3537a79642b1da3ab47fa3f1cbeb071311dda6b
mlaskowski7 <mtlaskowski7@gmail.com>
2023-12-11 23:00:13 +0100

    local storage js functionality, final commit

--- script.js
+++ script.js
@@ -6,10 +6,62 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
 
 function showModalFunc(){
     modal.classList.add('show-modal');
     websiteNameEl.focus();
+}
+
+function buildBookmarks(){
+    bookmarksContainer.textContent ='';
+    bookmarks.forEach((bookmark) =>{
+        const {name, url} = bookmark;
+        const item = document.createElement('div');
+        item.classList.add('item');
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fa-solid','fa-x');
+        closeIcon.setAttribute('title','Delete Bookmark');
+        closeIcon.setAttribute('onclick',`deleteBookmark('${url}')`);
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src',`https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute('alt','Favicon');
+        const link = document.createElement('a');
+        link.setAttribute('href',`${url}`);
+        link.setAttribute('target','_blank');
+        link.textContent = name;
+        linkInfo.append(favicon,link);
+        item.append(closeIcon,linkInfo);
+        bookmarksContainer.appendChild(item);
+    });
+}
+
+function fetchBookmarks(){
+    if(localStorage.getItem('bookmarks')){
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else{
+        bookmarks = [
+            {
+                name: 'Mateusz Laskowski',
+                url: 'https://mlaskowski7.github.io/Portfolio/',
+            },
+        ];
+        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    }
+    buildBookmarks();
+}
+
+function deleteBookmark(url){
+    bookmarks.forEach((bookmark, i) =>{
+        if(bookmark.url === url){
+            bookmarks.splice(i, 1);
+        }
+    });
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    fetchBookmarks();
+
 }
 
 function storeBookmark(event){
@@ -22,6 +74,16 @@ function storeBookmark(event){
     if(!validate(nameValue,urlValue)){
         return false;
     }
+    
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
     
 }
 
@@ -41,9 +103,7 @@ function validate(nameValue, urlValue){
         alert('Please submit the name value!');
         return false;
     }
-    if(urlValue.match(regex)){
-        alert('match');
-    } else{
+    if(!urlValue.match(regex)){
         alert('Provide a valid web address!');
         return false;
     }
@@ -52,3 +112,5 @@ function validate(nameValue, urlValue){
 }
 
 bookmarkForm.addEventListener('submit',storeBookmark);
+
+fetchBookmarks();
